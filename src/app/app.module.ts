db609a6dc52b07024ec467d6de3a655b1ad22ef3
Rodrigo Silva <rodrake@gmail.com>
2021-01-24 11:25:46 -0300

    Added ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './features/home/containers/home.module';
 import { CoreModule } from './core/core.module';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [CoreModule, AppRoutingModule, HomeModule],
+	imports: [CoreModule, AppRoutingModule, HomeModule, StoreModule.forRoot({}, {}), EffectsModule.forRoot([])],
 	bootstrap: [AppComponent]
 })
 export class AppModule {}
